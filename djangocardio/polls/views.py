9e7876c02dd8117a0f7f20af2db12576e7cf4902
FranciscoEP <francisco.eugp@gmail.com>
2022-06-13 22:17:57 -0500

    Routing added

--- djangocardio/polls/views.py
+++ djangocardio/polls/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta número {question_id}.")
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta número {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando en la pregunta número {question_id}.")
